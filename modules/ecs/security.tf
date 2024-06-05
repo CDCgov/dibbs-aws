b0110ec1d54b4aa729b068f4c1c2bebb440af01a
marycrawford <mary.crawford@skylight.digital>
2024-06-05 14:39:33 -0700

    add orchestration service sg

--- modules/ecs/security.tf
+++ modules/ecs/security.tf
@@ -8,6 +8,35 @@ resource "aws_security_group" "alb_sg" {
     protocol    = "tcp"
     from_port   = var.app_port
     to_port     = var.app_port
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  #matches the load balancer listener rule (without unreachable)
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "tcp"
+    from_port   = 8080 #changed from https port 443
+    to_port     = 8080 #changed from https port 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "orchestration_sg" {
+  name        = "dibbs-aws-ecs-orchestration-security-group"
+  description = "controls access to the orchestration service"
+  vpc_id      = var.vpc_id
+
+
+  #matches the load balancer listener rule (without unreachable)
+  ingress {
+    protocol    = "tcp"
+    from_port   = var.app_port
+    to_port     = var.app_port
     cidr_blocks = ["${var.cidr}"]
   }
   #matches the load balancer listener rule (without unreachable)
@@ -20,8 +49,8 @@ resource "aws_security_group" "alb_sg" {
 
   egress {
     protocol    = "tcp"
-    from_port   = 8080 #changed from https port 443
-    to_port     = 8080 #changed from https port 443
+    from_port   = 8080
+    to_port     = 8080
     cidr_blocks = ["${var.cidr}"]
   }
 }
