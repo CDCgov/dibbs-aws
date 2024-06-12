7dd0805ee567ab7523e96c893400311b20721e73
marycrawford <mary.crawford@skylight.digital>
2024-06-12 13:05:44 -0700

    remove comment out code

--- modules/ecr/outputs.tf
+++ modules/ecr/outputs.tf
@@ -4,8 +4,4 @@ output "repository_url" {
   value = [for repo in aws_ecr_repository.repo : repo.repository_url]
 }
 
-# output "repository_urls" {
-#   value = { for key, repo in aws_ecr_repository.repo : key => repo.repository_url }
-# }
-
 
