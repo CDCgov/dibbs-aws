aab349dd9eed579f0656b3cbbd901bedb9c16869
Alis Akers <alismxwll@gmail.com>
2025-02-13 12:57:24 -0800

    feat: remove ephemeral environment workflow and update ECS related configurations

--- terraform/implementation/ecs/main.tf
+++ terraform/implementation/ecs/main.tf
@@ -49,34 +49,34 @@ module "ecs" {
   tags         = local.tags
   phdi_version = var.phdi_version
 
-  ## The following variables will need to be configured depending on your requirements
+  # The following variables will need to be configured depending on your requirements
 
   # If intent is to pull from the dibbs-ecr-viewer GHCR, set disable_ecr to true (default is false when not set)
   disable_ecr = false
 
-  ## If the intent is to make the ecr-viewer availabble on the public internet, set internal to false (default is true when not set)
-  ## This requires an internet gateway to be present in the VPC.
+  # If the intent is to make the ecr-viewer availabble on the public internet, set internal to false (default is true when not set)
+  # This requires an internet gateway to be present in the VPC.
   internal = var.internal
 
-  ## If the intent is to enable https and port 443, pass the arn of the cert in AWS certificate manager. This cert will be applied to the load balancer. (default is "" when not set)
+  # If the intent is to enable https and port 443, pass the arn of the cert in AWS certificate manager. This cert will be applied to the load balancer. (default is "" when not set)
   certificate_arn = data.aws_acm_certificate.this.arn
 
-  ## To disable autoscaling, set enable_autoscaling to false (default is true when not set)
+  # To disable autoscaling, set enable_autoscaling to false (default is true when not set)
   enable_autoscaling = true
 
-  ## If the intent is to disable authentication, set nbs_auth to false (default is true when not set)
+  # If the intent is to disable authentication, set nbs_auth to false (default is true when not set)
   nbs_auth = false
 
-  ## If intent is to use a metadata database for the ecr-viewer library, provider the required secrets manager names
+  # If intent is to use a metadata database for the ecr-viewer library, provider the required secrets manager names
   # Postgresql database example (default is "" when not set)
   secrets_manager_postgresql_connection_string_name = module.db.secrets_manager_postgresql_connection_string_name
 
-  ## SqlServer database example (default values are "" when not set)
+  # SqlServer database example (default values are "" when not set)
   # secrets_manager_sqlserver_user_name = module.db.secrets_manager_sqlserver_user_name
   # secrets_manager_sqlserver_password_name = module.db.secrets_manager_sqlserver_password_name
   # secrets_manager_sqlserver_host_name = module.db.secrets_manager_sqlserver_host_name
 
-  ## dibbs_config_name can be a value found here under CONFIG_NAME: https://github.com/CDCgov/dibbs-ecr-viewer/blob/main/containers/ecr-viewer/environment.d.ts
-  ## This is used to configure the ecr-viewer application. (default is "" when not set)
+  # dibbs_config_name can be a value found here under CONFIG_NAME: https://github.com/CDCgov/dibbs-ecr-viewer/blob/main/containers/ecr-viewer/environment.d.ts
+  # This is used to configure the ecr-viewer application. (default is "" when not set)
   dibbs_config_name = "AWS_PG_NON_INTEGRATED"
 }
