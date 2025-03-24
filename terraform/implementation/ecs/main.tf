7222edde625b86c4e6ee8213e4533f4c18774941
shanice-skylight <150187231+shanice-skylight@users.noreply.github.com>
2025-03-24 14:03:30 -0400

    Added Orchestration URL (#58)
    * updated source for testing
    
    * modified source to correct format
    
    * modified source and version

--- terraform/implementation/ecs/main.tf
+++ terraform/implementation/ecs/main.tf
@@ -38,7 +38,7 @@ module "db" {
 
 module "ecs" {
   source  = "CDCgov/dibbs-ecr-viewer/aws"
-  version = "0.5.0"
+  version = "0.5.1"
 
   public_subnet_ids  = flatten(module.vpc.public_subnets)
   private_subnet_ids = flatten(module.vpc.private_subnets)
