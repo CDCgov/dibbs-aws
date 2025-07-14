c640773e4ce5d1df62751bc76ef3682610a82c0a
Alis Akers <alismxwll@gmail.com>
2025-07-14 10:01:29 -0700

    fix: wrap secret string retrievals with try() in secrets_manager_database_connection_string_version output

--- terraform/modules/db/_output.tf
+++ terraform/modules/db/_output.tf
@@ -1,4 +1,4 @@
 output "secrets_manager_database_connection_string_version" {
-  value     = var.database_type == "postgresql" ? aws_secretsmanager_secret_version.postgresql[0].secret_string : var.database_type == "sqlserver" ? aws_secretsmanager_secret_version.sqlserver_connection_string[0].secret_string : ""
+  value     = var.database_type == "postgresql" ? try(aws_secretsmanager_secret_version.postgresql[0].secret_string) : var.database_type == "sqlserver" ? try(aws_secretsmanager_secret_version.sqlserver_connection_string[0].secret_string) : ""
   sensitive = true
 }
