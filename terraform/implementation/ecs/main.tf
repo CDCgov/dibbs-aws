26abd20acebba8649a71a1edd6ae111a00249d40
shanice-skylight <shanice@skylight.digital>
2025-12-17 11:14:29 -0500

    fixed typo

--- terraform/implementation/ecs/main.tf
+++ terraform/implementation/ecs/main.tf
@@ -39,7 +39,7 @@ module "ecs" {
   # source  = "CDCgov/dibbs-ecr-viewer/aws"
   # version = "0.11.0"
   # github branch source 
-  source = "git::https://github.com/CDCgov/terraform-aws-dibbs-ecr-viewer.git?ref=shanice/fix_env_var_erro"
+  source = "git::https://github.com/CDCgov/terraform-aws-dibbs-ecr-viewer.git?ref=shanice/fix_env_var_error"
 
   public_subnet_ids  = flatten(module.vpc.public_subnets)
   private_subnet_ids = flatten(module.vpc.private_subnets)
