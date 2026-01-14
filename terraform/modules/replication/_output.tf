68465e2f2408eb5a6a3a67f62af38c1d5228b04b
Alis Akers <alismxwll@gmail.com>
2026-01-14 09:06:21 -0800

    chore: comment out unused replication resources and outputs

--- terraform/modules/replication/_output.tf
+++ terraform/modules/replication/_output.tf
@@ -1,7 +1,7 @@
-output "s3_replication_bucket_arn" {
-  value = aws_s3_bucket.replication.arn
-}
+# output "s3_replication_bucket_arn" {
+#   value = aws_s3_bucket.replication.arn
+# }
 
-output "s3_replication_bucket_name" {
-  value = aws_s3_bucket.replication.bucket
-}
+# output "s3_replication_bucket_name" {
+#   value = aws_s3_bucket.replication.bucket
+# }
