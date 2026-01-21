aaefa59fb09b0870af079ec40e522688af9604eb
Alis Akers <alismxwll@gmail.com>
2026-01-20 16:41:01 -0800

    chore: remove unused replication module files

--- terraform/modules/replication/_output.tf
+++
@@ -1,7 +0,0 @@
-# output "s3_replication_bucket_arn" {
-#   value = aws_s3_bucket.replication.arn
-# }
-
-# output "s3_replication_bucket_name" {
-#   value = aws_s3_bucket.replication.bucket
-# }
--- terraform/modules/replication/_variable.tf
+++
@@ -1,5 +0,0 @@
-variable "s3_replication_bucket_name" {
-  description = "Name of the S3 bucket for replication"
-  type        = string
-  default     = ""
-}
